8acaf6d9e692f9afdf168f6a8cde502fb3d981ad
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-11-25 06:39:15 +0800

    init project react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [columns, setColumns] = useState([]);
+  const [records, setRecords] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3030/users").then((res) => {
+      setColumns(Object.keys(res.data[0]));
+      setRecords(res.data);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((c, i) => (
+              <th key={i}>{c}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 }
