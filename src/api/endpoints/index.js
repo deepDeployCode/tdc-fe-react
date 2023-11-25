fc081ce1282c758ed6ed291794be843dbc78b697
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-11-25 12:17:06 +0800

    add api crud user

--- src/api/endpoints/index.js
+++ src/api/endpoints/index.js
@@ -10,6 +10,15 @@ const methods = {
   listData() {
     return resource.get(`${endpoint.index.list}`);
   },
+  storeData(data) {
+    return resource.post(`${endpoint.index.list}`);
+  },
+  updateData(id, data) {
+    return resource.put(`${endpoint.index.list}`);
+  },
+  deleteData(id, data) {
+    return resource.delete(`${endpoint.index.list}`);
+  },
 };
 
 export default methods;
