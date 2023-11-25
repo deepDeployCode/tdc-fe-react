317eacdfa1133cbfb9c9e3a9340310a019261cfb
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-11-25 12:51:29 +0800

    fix consume api create data user

--- src/view/Add.jsx
+++ src/view/Add.jsx
@@ -1,56 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api/endpoints";
 
 function Add() {
+  const [inputData, setInputData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+  const redirectTo = useNavigate(); //method redirect hook to routerdom
+  function handlerCreateData(event) {
+    event.preventDefault();
+    api
+      .storeData(inputData)
+      .then((res) => {
+        alert("Successfully Create Data");
+        redirectTo("/");
+      })
+      .catch((error) => console.log(error.response));
+  }
   return (
     <div className="d-flex w-100 justify-content-center align-items-center">
       <div className="w-50 border bg-light p-5">
         <center>
           <img src={img} alt="logo img" width={150} height={150} />
         </center>
-        <form>
+        <form onSubmit={handlerCreateData}>
           <div className="form-group">
-            <label for="exampleInputEmail1">Name</label>
+            <label id="name">Name</label>
             <input
-              type="email"
+              type="text"
               className="form-control"
-              id="exampleInputEmail1"
+              id="name"
               aria-describedby="emailHelp"
               placeholder="Enter Name"
+              name="name"
+              onChange={(e) =>
+                setInputData({ ...inputData, name: e.target.value })
+              }
+              required
             ></input>
           </div>
           <br />
           <div className="form-group">
-            <label for="exampleInputEmail1">Email</label>
+            <label id="email">Email</label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="email"
               aria-describedby="emailHelp"
               placeholder="Enter Email"
+              name="email"
+              onChange={(e) =>
+                setInputData({ ...inputData, email: e.target.value })
+              }
+              required
             ></input>
           </div>
           <br />
           <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label id="password">Password</label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputEmail1"
+              id="password"
               aria-describedby="emailHelp"
               placeholder="Enter Password"
+              name="password"
+              onChange={(e) =>
+                setInputData({ ...inputData, password: e.target.value })
+              }
+              required
             ></input>
           </div>
           <br />
           <div className="form-group">
-            <label for="exampleInputPassword1">Password Confirmation</label>
+            <label id="password_confirmation">Password Confirmation</label>
             <input
               type="password"
               className="form-control"
-              id="exampleInputEmail1"
+              id="password_confirmation"
               aria-describedby="emailHelp"
               placeholder="Enter Password Confirmation"
+              name="password_confirmation"
+              onChange={(e) =>
+                setInputData({
+                  ...inputData,
+                  password_confirmation: e.target.value,
+                })
+              }
+              required
             ></input>
           </div>
           <br />
