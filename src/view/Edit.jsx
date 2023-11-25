bbf24b9b240ad276b184c1f429ba389cb522dd5b
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-11-25 14:31:51 +0800

    fix update data

--- src/view/Edit.jsx
+++ src/view/Edit.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import img from "../logo.svg";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../api/endpoints";
 
 function Edit() {
   const { id } = useParams();
   const [data, setData] = useState([]);
+  const RedirectTo = useNavigate();
   useEffect(() => {
     api
       .detailData(id)
@@ -14,25 +15,36 @@ function Edit() {
         setData(res.data);
       })
       .catch((error) => console.log(error));
-  });
+  }, []);
+  function handlerUpdate(event) {
+    event.preventDefault();
+    api
+      .updateData(id, data)
+      .then((res) => {
+        alert("Successfully Update Data");
+        RedirectTo("/");
+      })
+      .catch((error) => console.log(error.response));
+  }
   return (
     <div className="d-flex w-100 justify-content-center align-items-center">
       <div className="w-50 border bg-light p-5">
         <center>
           <img src={img} alt="logo img" width={150} height={150} />
         </center>
-        <form>
+        <form onSubmit={handlerUpdate}>
           <div className="form-group">
             <label id="name">Name</label>
             <input
               type="text"
+              value={data.name}
               className="form-control"
               id="name"
               aria-describedby="emailHelp"
               placeholder="Enter Name"
               name="name"
               required
-              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })}
             ></input>
           </div>
           <br />
@@ -40,13 +52,14 @@ function Edit() {
             <label id="email">Email</label>
             <input
               type="email"
+              value={data.email}
               className="form-control"
               id="email"
               aria-describedby="emailHelp"
               placeholder="Enter Email"
               name="email"
               required
-              value={data.email}
+              onChange={(e) => setData({ ...data, email: e.target.value })}
             ></input>
           </div>
           <br />
@@ -59,7 +72,7 @@ function Edit() {
               aria-describedby="emailHelp"
               placeholder="Enter Password"
               name="password"
-              required
+              onChange={(e) => setData({ ...data, password: e.target.value })}
             ></input>
           </div>
           <br />
@@ -72,7 +85,9 @@ function Edit() {
               aria-describedby="emailHelp"
               placeholder="Enter Password Confirmation"
               name="password_confirmation"
-              required
+              onChange={(e) =>
+                setData({ ...data, password_confirmation: e.target.value })
+              }
             ></input>
           </div>
           <br />
