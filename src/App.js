6343ff97e8a5e61eed3670cdc3f999c27415a810
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-11-25 10:51:07 +0800

    fix api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../src/api/endpoints";
+import api from "./api/endpoints";
 
 function App() {
   const [columns, setColumns] = useState([]);
